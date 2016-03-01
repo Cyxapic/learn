f051747e74bd55fd92ff61a9015379f18d5aad6c
Artem Sukharenko <+79197703910@yandex.ru>
2016-03-01 18:41:57 +0300

    yyy

--- thred_step_serv.py
+++ thred_step_serv.py
@@ -1,24 +1,27 @@
-import socket, threading                                                        
-                                                                                
-class talkToClient(threading.Thread):                                           
-    def __init__(self, clientSock, addr):                                       
-        self.clientSock = clientSock                                             
-        self.addr = addr                                                        
-        threading.Thread.__init__(self)                                         
-    def run(self):                                                              
-        while True:                                                             
-            data = self.clientSock.recv(1024)                                    
-            if not data:                                                        
-                self.clientSock.send('bye')                                     
-                break                                                           
-            self.clientSock.send(data)                                          
-            if data == 'close':                                                 
-                break                                                           
-        self.clientSock.close()                                                 
-conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                        
-conn.bind(('0.0.0.0', 2222))
-conn.listen(10)                                                                 
-while True:                                                                     
-    channel, details = conn.accept()                                            
-    talkToClient(channel, details).start()                                      
-conn.close()
+class EchoHandler(asyncore.dispatcher_with_send):
+
+    def handle_read(self):
+        data = self.recv(1024)
+        if data:
+            if data == 'close' or data == 'Close':
+                self.close()
+            else: self.send(data)
+
+class EchoServer(asyncore.dispatcher):
+
+    def __init__(self, host, port):
+        asyncore.dispatcher.__init__(self)
+        self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.set_reuse_addr()
+        self.bind((host, port))
+        self.listen(10)
+
+    def handle_accept(self):
+        pair = self.accept()
+        if pair is not None:
+            sock, addr = pair
+            print 'Incoming connection from %s' % repr(addr)
+            handler = EchoHandler(sock)
+
+server = EchoServer('0.0.0.0', 2222)
+asyncore.loop()
